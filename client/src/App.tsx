b68fdced826cbf960e090d48afd7c7d2fbfebd01
Adrianna V. <adrianna.valdivia@salesloft.com>
2024-06-16 22:15:59 -0400

    update main page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CommentGroup, Header } from 'semantic-ui-react';
+
 import CommentForm from './CommentForm';
 import CommentFeed from './CommentFeed';
-import logo from './logo.svg';
 import './App.css';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className='App'>
+    <div className='comment-layout'>
       <QueryClientProvider client={queryClient}>
         <div>
           <h1>Comments</h1>
-          <CommentForm />
-          <CommentFeed />
+          <CommentGroup>
+            <CommentFeed />
+            <div className='comment-layout-divider'></div>
+            <CommentForm />
+          </CommentGroup>
         </div>
       </QueryClientProvider>
     </div>
